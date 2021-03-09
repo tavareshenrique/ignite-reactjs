3cd34db08b25c39d9b7c2dc3a6e9ee38008dc48f
Henrique Tavares <ihenrits@gmail.com>
2021-03-09 06:23:25 -0300

    estrutura do ReactJS

--- 01-github-explorer/babel.config.js
+++ 01-github-explorer/babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    "@babel/preset-env",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+  ],
 };
--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: path.resolve(__dirname, "src", "index.jsx"),
 
   output: {
