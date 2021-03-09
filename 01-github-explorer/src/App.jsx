f78a79f75efb637c1d91ce82a1ba10263d726371
Henrique Tavares <ihenrits@gmail.com>
2021-03-09 07:03:17 -0300

    primeiro componente react

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,5 +1,7 @@
+import { RepositoryList } from "./components/RepositoryList";
+
 import "./styles/global.scss";
 
 export function App() {
-  return <h1>Hello World</h1>;
+  return <RepositoryList />;
 }
---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,32 @@
+const repositoryName = "unform2";
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar Repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar Repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar Repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
