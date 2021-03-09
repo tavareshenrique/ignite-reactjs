d78906e26a1ac9e63f46c0c9342e042ace8aed2a
Henrique Tavares <ihenrits@gmail.com>
2021-03-09 06:31:20 -0300

    utilizando dev server

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
