535da911e5e6afcae2ab70917804ae468284e1ef
Henrique Tavares <ihenrits@gmail.com>
2021-03-08 22:00:07 -0300

    adicionando github-explorer

---
+++ 01-github-explorer/dist/bundle.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello Ignite");
+}
---
+++ 01-github-explorer/src/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+function App() {
+  return <h1>Hello Ignite</h1>;
+}
