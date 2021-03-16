4f495e242b55b75ad697771d7fe1321386be210a
Henrique Tavares <ihenrits@gmail.com>
2021-03-16 07:02:33 -0300

    fontes do google fonts

--- 02-dtmoney/src/styles/global.ts
+++ 02-dtmoney/src/styles/global.ts
@@ -36,6 +36,15 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+  
+  h1,h2,h3,h4,h5, h5 {
+    font-weight: 600;
+  }
+
   button {
     cursor: pointer;
   }
