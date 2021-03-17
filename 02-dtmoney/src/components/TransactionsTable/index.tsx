bda19a3b8de4e85808a2ff55ec9a138f20809b3e
Henrique Tavares <ihenrits@gmail.com>
2021-03-17 07:04:34 -0300

    configurando MirageJS

--- 02-dtmoney/src/components/TransactionsTable/index.tsx
+++ 02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transactions")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <Container>
       <table>
--- 02-dtmoney/src/index.tsx
+++ 02-dtmoney/src/index.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
 import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
