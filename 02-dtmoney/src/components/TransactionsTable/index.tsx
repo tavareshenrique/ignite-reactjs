b376fc018f772750b8a46cfe8b468e95ea066259
Henrique Tavares <ihenrits@gmail.com>
2021-03-18 07:37:22 -0300

    formatando valores

--- 02-dtmoney/src/components/TransactionsTable/index.tsx
+++ 02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -30,9 +30,18 @@ export function TransactionsTable() {
           {transactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{transaction.amount}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
