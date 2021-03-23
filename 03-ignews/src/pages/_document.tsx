25a3020c6271654698d6af4c0d0534bb7d343156
Henrique Tavares <ihenrits@gmail.com>
2021-03-23 06:46:27 -0300

    title dinamico por pagina

--- 03-ignews/src/pages/_document.tsx
+++ 03-ignews/src/pages/_document.tsx
@@ -10,8 +10,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
-
-          <title>ig.news</title>
         </Head>
         <body>
           <Main />
--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,3 +1,13 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <h1>Hello World</h1>
+    </>
+  );
 }
