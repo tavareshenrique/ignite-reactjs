ef411ca5ae9618ee8eb20a76f7a49380f904b996
Henrique Tavares <ihenrits@gmail.com>
2021-03-23 07:39:33 -0300

    pagina home

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,13 +1,28 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all he publications <br />
+            <span>for $9,90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
