5b265d7ad567f99a1da306b2400b543717766e6c
Henrique Tavares <ihenrits@gmail.com>
2021-03-27 14:47:51 -0300

    redirecionando para o Stripe

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,8 @@
 import { signin, useSession } from "next-auth/client";
+
+import api from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+
 import { ISubscribeButtonProps } from "./@interfaces";
 
 import styles from "./styles.module.scss";
@@ -6,10 +10,22 @@ import styles from "./styles.module.scss";
 export function SubscribeButton({ priceId }: ISubscribeButtonProps) {3
   const [session] = useSession();
 
-  function handleSubscribe() {
+   async function handleSubscribe() {
     if (!session) {
       signin('github');
       return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe  = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message)
     }
   }
 
---
+++ 03-ignews/src/services/stripe-js.ts
@@ -0,0 +1,7 @@
+import { loadStripe } from '@stripe/stripe-js';
+
+export async function getStripeJs() {
+  const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
+
+  return stripeJs
+}
