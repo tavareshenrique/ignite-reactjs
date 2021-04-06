33466f54a4a51a5d8dc7bd80b3eb25f6495aa071
Henrique Tavares <ihenrits@gmail.com>
2021-04-06 07:32:43 -0300

    adicionando eslint, prettier e editorconfig

--- 04-dashgo/src/pages/_app.tsx
+++ 04-dashgo/src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
-import { theme } from '../styles/theme';
+import { theme } from 'styles/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ChakraProvider theme={theme} >
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,7 +1,3 @@
-import Head from 'next/head'
-
-export default function Home() {
-  return (
-   <h1>Dashgo</h1>
-  )
+export default function Home(): JSX.Element {
+  return <h1>Dashgo</h1>;
 }
