34da9b3951e1ac74d0dd6d5a97094d515da5896a
Henrique Tavares <ihenrits@gmail.com>
2021-04-06 21:30:02 -0300

    finalizando o Header

--- 04-dashgo/src/components/Header/index.tsx
+++ 04-dashgo/src/components/Header/index.tsx
@@ -1,5 +1,9 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from 'react-icons/ri';
 
 export function Header(): JSX.Element {
   return (
@@ -43,6 +47,37 @@ export function Header(): JSX.Element {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Henrique Tavares</Text>
+            <Text color="gray.300" fontSize="small">
+              ihenrits@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Henrique Tavares"
+            src="https://github.com/tavareshenrique.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
