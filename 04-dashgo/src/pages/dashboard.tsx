aad3da4e3059018b8556d083c17ebe519e2d30ae
Henrique Tavares <ihenrits@gmail.com>
2021-04-06 21:48:34 -0300

    componente: Sidebar

--- 04-dashgo/src/pages/dashboard.tsx
+++ 04-dashgo/src/pages/dashboard.tsx
@@ -1,5 +1,16 @@
+import { Flex } from '@chakra-ui/react';
+
 import { Header } from 'components/Header';
+import { Sidebar } from 'components/Sidebar';
 
 export default function Dashboard(): JSX.Element {
-  return <Header />;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth="1480" mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 }
