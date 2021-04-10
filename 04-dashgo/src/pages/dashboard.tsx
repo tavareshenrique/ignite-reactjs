3058b98a368f52b42b49d2d2a0177a668f10bd06
Henrique Tavares <ihenrits@gmail.com>
2021-04-10 20:06:08 -0300

    Dashboard responsivo

--- 04-dashgo/src/pages/dashboard.tsx
+++ 04-dashgo/src/pages/dashboard.tsx
@@ -71,7 +71,7 @@ export default function Dashboard(): JSX.Element {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
