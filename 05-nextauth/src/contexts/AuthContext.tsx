954dc38d797c8a6cf7b82b1bb7f6fe655f679f7b
Henrique Tavares <ihenrits@gmail.com>
2021-06-29 15:33:58 -0300

    configurando cliente do axios

--- 05-nextauth/src/contexts/AuthContext.tsx
+++ 05-nextauth/src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from 'react';
+import { api } from '../services/api';
 
 type SignInCredentials = {
   email: string;
@@ -20,7 +21,13 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const isAuthenticated = false;
 
   async function signIn({ email, password }: SignInCredentials) {
-    console.log({ email, password })
+    try {
+      const response = await api.post('sessions', { email, password });
+
+      console.log('response', response);
+    } catch (err) {
+      console.log('err', err);
+    }
   }
 
   return (
