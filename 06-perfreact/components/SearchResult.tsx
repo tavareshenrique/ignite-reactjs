b65cedd976aed2e3aaaf350e0225e3c2a59acf4b
Henrique Tavares <ihenrits@gmail.com>
2021-07-07 00:51:31 -0300

    useMemo

--- 06-perfreact/components/SearchResult.tsx
+++ 06-perfreact/components/SearchResult.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import { ProductItem } from './ProductItem';
 
@@ -11,11 +11,26 @@ interface ISearchResultsProps {
 }
 
 export function SearchResult({ results }: ISearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, prtoduct) => {
+      return total + prtoduct.price
+    }, 0);
+  }, [results])
+
   return (
     <div>
+      <h2>Total Price: {totalPrice}</h2>
+
       {results.map((product) => (
         <ProductItem key={product.id} product={product}  />
       ))}
     </div>
   )
 }
+
+/**
+ * Quando usar o useMemo?
+ * 
+ * 1. Para cálculos pesados;
+ * 2. Igualdade referencial (quando repassa aquela informação para um componente filho);
+ */
