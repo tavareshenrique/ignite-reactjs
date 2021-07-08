932aae0e9cf4c9b9a5ffe298e8b4de73f4cdb4a8
Henrique Tavares <ihenrits@gmail.com>
2021-07-08 14:51:52 -0300

    testando primeiro component

--- 03-ignews/jest.config.js
+++ 03-ignews/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testIgnore: ['/node_modules/', '/.next/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
---
+++ 03-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,35 @@
+import { render } from '@testing-library/react';
+
+import { ActiveLink } from '.';
+
+jest.mock('next/router', () => {
+  return {
+    useRouter() {
+      return {
+        asPath: '/',
+      };
+    },
+  };
+});
+
+describe('ActiveLink Component', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>,
+    );
+
+    expect(getByText('Home')).toBeInTheDocument();
+  });
+
+  it('adds active class if the link as currently active', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>,
+    );
+
+    expect(getByText('Home')).toHaveClass('active');
+  });
+});
