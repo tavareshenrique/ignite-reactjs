a8891fc3120351ae511d503920b3dedc1a16dc5c
Henrique Tavares <ihenrits@gmail.com>
2021-07-07 21:18:05 -0300

    bundle analyzer

--- 06-perfreact/next.config.js
+++ 06-perfreact/next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({})
+
+// ANALYZE=true yarn build
